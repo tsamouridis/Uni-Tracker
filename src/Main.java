3a5df5c3941e5485e2a50343c45ccdbb0ef731a5
tsamourid <thanasistsam@yahoo.com>
2021-07-24 01:00:20 +0300

    Edit main to create a new Gui object

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        new Gui();
+    
         // Semester first = new Semester(1, 5);
 
         // System.out.println("___________________SEMESTER 1________________");
