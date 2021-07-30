aa6c7bf61c19c2b29cd88720218c77bab8a6a388
tsamourid <thanasistsam@yahoo.com>
2021-07-30 16:16:47 +0300

    Delete not necessary imports

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,6 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.BufferedReader;  
 import java.io.FileReader; 
 import java.io.IOException;  
-import java.io.PrintWriter;
 
 public class Main {
     public static void main(String[] args) {
